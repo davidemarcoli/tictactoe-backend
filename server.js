616cd5308ce3a41229974832f427263120ebbeac
Davide Marcoli <davide.marcoli@noseryoung.ch>
2021-12-14 16:55:48 +0100

    console logs for new rooms

--- server.js
+++ server.js
@@ -4,35 +4,32 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var players = 0;
+var currentRoom = 1;
 
 io.on('connection', (socket) => {
     players++;
 
     console.log('a user connected');
 
-    socket.join('room1');
+    socket.join('room' + currentRoom);
+    let room = 'room' + currentRoom;
 
-    if (players > 1) {
-        console.log(players + " Players!");
-        // io.to(socket).emit('changePlayer');
-        // setTimeout(() => {  io.to(socket).emit('changePlayer'); }, 1000);
-        socket.broadcast.emit('changePlayer');
+    if (players === 2) {
+        players = 0;
+        currentRoom++;
+        socket.to(room).emit('changePlayer');
+        console.log("New Room: " + 'room' + currentRoom);
     }
 
-    // socket.on('message', (msg) => {
-    //     console.log(msg);
-    //     socket.broadcast.emit('message-broadcast', msg);
-    // });
     socket.on('position', (position) => {
-        socket.broadcast.emit('position-broadcast', position);
+        socket.to(room).emit('position-broadcast', position);
     });
     socket.on('changePlayer', () => {
-        // socket.broadcast.to("room1").emit('changePlayer');
-        io.in("room1").emit('changePlayer');
+        io.in(room).emit('changePlayer');
         console.log("changePlayer");
     });
     socket.on('newGame', () => {
-        io.in("room1").emit('newGame');
+        io.in(room).emit('newGame');
     });
     socket.on('disconnect', () => {
         players--;
