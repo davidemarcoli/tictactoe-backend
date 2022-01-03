27209bb880553563862aec162b0715e74e859317
Davide Marcoli <davide@marcoli.ch>
2022-01-03 14:04:22 +0100

    better port printing

--- server.js
+++ server.js
@@ -88,5 +88,5 @@ io.on('connection', (socket) => {
 
 app.get('/', (req, res) => res.send('hello!'));
 http.listen(process.env.PORT || 3000, () => {
-    console.log('listening on *:3000');
+    console.log('listening on *:' + (process.env.PORT || 3000));
 });
